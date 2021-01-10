ec5dcf8214e127d1967a2fa29bb0f99dc8807701
Ahmed Tayel <ahmed.tayel97@gmail.com>
2021-01-10 03:55:49 +0200

    Parses a recipe and adds to a database successfully with correct associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,30 +2,25 @@ class Ingredient < ApplicationRecord
   has_many :recipes_ingredients
   has_many :recipes, through: :recipes_ingredients
 
-  attr_accessor :ingredient, :length
+  attr_accessor :ingredient_token, :length
   #Initializes an instance thats takes an entry from the ingredients as a string and tokenizes it
   def self.newInstance ingredient_string
     ingredient = self.new
-    ingredient.ingredient = ingredient_string.split(" ")
-    ingredient.length =  ingredient.ingredient.length
+    ingredient.ingredient_token = ingredient_string.split(" ")
+    ingredient.length =  ingredient.ingredient_token.length
     ingredient
   end
 
   #Returns the first two tokens assumed to be an amount and a unit
   def get_amount 
-    @ingredient.shift(2).join(" ")
+    @amount = @ingredient_token.shift(2).join(" ")
   end
   #Returns the remaining tokens
   def get_name 
-    @ingredient.pop(@length).join(" ")
+    @name = @ingredient_token.pop(@length).join(" ")
   end
-
+  #Returns the ingredient object from the database if found
   def exists?
-    if  Ingredient.find_by(name: "#{@ingredient.get_name}")
-      
-    else
-      
-    end
-   
+    Ingredient.find_by(name: "#{@name}")
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,22 +7,29 @@ class Recipe < ApplicationRecord
     data = HTMLParser.load url
     recipe = Recipe.create(title: "#{data[:title]}", description: "#{data[:description]}")
     data[:ingredients].each do |ingredient| 
+
       ingredient_entry =  Ingredient.newInstance ingredient
 
-      #The algorithm breaks if you called #get_name before #get_amount
+      # The algorithm breaks if you called #get_name before #get_amount
       ingredient_amount = ingredient_entry.get_amount 
       ingredient_name = ingredient_entry.get_name
-      #Do not change the order of line 12 and 13
+      # Do not change the order of line 12 and 13
+      
+      
+      ingredient_found = ingredient_entry.exists?
 
-      ingredient_entry.exists? 
-      unless ingredient_found.nil? 
+      if ingredient_found 
+        # Connected the recipe to the ingredient in the join table and adds the amount associated with it
         RecipesIngredient.create(recipe_id: "#{recipe[:id]}", ingredient_id:"#{ingredient_found[:id]}", quantity: "#{ingredient_amount}")
       else
+        # Creates the ingredient in the database, and add the connection between the recipe and the ingredient in the join table
+        # and adds the amount associated with it
         placebo = Ingredient.create(name: "#{ingredient_name}")
-        RecipesIngredient.create(recipe_id: "#{recipe[:id]}", ingredient_id:"#{placebo[:id]}")
+        RecipesIngredient.create(recipe_id: "#{recipe[:id]}", ingredient_id:"#{placebo[:id]}", quantity: "#{ingredient_amount}")
       end
+
     end
   end
-    
 
 end
+  
